4e98a2c216e6b5c33b569c774a2a630e7747410a
Sara <sape0003@stud.kea.dk>
2024-10-11 21:53:30 +0200

    Major changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,11 +6,29 @@ export default {
       animation: {
         "infinite-scroll": "infinite-scroll 60s linear infinite",
       },
+      colors: {
+        "azure-radiance": {
+          50: "#eef7ff",
+          100: "#d8edff",
+          200: "#badfff",
+          300: "#8bcdff",
+          400: "#54b0ff",
+          500: "#2c8dff",
+          600: "#2175fb",
+          700: "#0e57e7",
+          800: "#1246bb",
+          900: "#153f93",
+          950: "#122759",
+        },
+      },
       keyframes: {
         "infinite-scroll": {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(-100%)" },
         },
+      },
+      cursor: {
+        handcursor: "url(/cursor.png) 25 25, pointer",
       },
     },
   },
